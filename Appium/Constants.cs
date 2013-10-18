4088269a70d07c882be701e37a9084a0c230aa21
unknown <danc@danc-auto-dev.zoosk.local>
2013-10-18 08:08:47 -0700

    0.10.4

--- Appium/Constants.cs
+++ Appium/Constants.cs
@@ -3,8 +3,8 @@
     public static class Constants
     {
         #region Urls
-        public const string NodeJSWindowsBinaryUrl = "http://nodejs.org/dist/v0.10.18/node.exe";
-        public const string NPMWindowsZipUrl = "http://nodejs.org/dist/npm/npm-1.3.9.zip";
+        public const string NodeJSWindowsBinaryUrl = "http://nodejs.org/dist/v0.10.20/node.exe";
+        public const string NPMWindowsZipUrl = "http://nodejs.org/dist/npm/npm-1.3.11.zip";
         #endregion
     }
 }
