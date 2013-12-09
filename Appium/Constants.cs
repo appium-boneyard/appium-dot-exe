4bb421c676c9aff0129e20ae2635d99496aebd14
Dan Cuellar <penguinho@aol.com>
2013-12-09 07:50:34 -0800

    0.12.2

--- Appium/Constants.cs
+++ Appium/Constants.cs
@@ -3,7 +3,7 @@
     public static class Constants
     {
         #region Urls
-        public const string NodeJSWindowsBinaryUrl = "http://nodejs.org/dist/v0.10.21/node.exe";
+        public const string NodeJSWindowsBinaryUrl = "http://nodejs.org/dist/v0.10.17/node.exe";
         public const string NPMWindowsZipUrl = "http://nodejs.org/dist/npm/npm-1.3.13.zip";
         #endregion
     }
