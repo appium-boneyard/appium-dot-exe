4bf334422a73be500738f5d9af6fcee1986957c5
Astro03 <locopelon03@hotmail.com>
2015-03-30 12:11:06 -0700

    auto update working fully, updating application and restarting it

--- installer/update.bat
+++ installer/update.bat
@@ -1,11 +1,11 @@
 :: use installer to install appium 
-appium-installer.exe /SP- /silent /noicons /closeapplications /dir=expand:"%1"
+appium-installer.exe /SP- /silent /noicons /closeapplications /dir=expand:%1
 
 :: delete the installer 
 del appium-installer.exe
 
 :: start the appium application
-Appium.exe
+::Appium.exe  :: no longer need to launch the application since the installer starts the application automatically
 
 :: delete update.bat file 
 call :deleteSelf&exit /b
