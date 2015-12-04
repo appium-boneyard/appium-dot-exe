4ac94deb5c5b09265516cc6a573f5d8cf7ea3794
Peter Vermeulen <peetvermeulen@gmail.com>
2015-12-04 21:06:00 +0100

    Upgrading to newest node.js

--- AppiumWPF/Utility/Constants.cs
+++ AppiumWPF/Utility/Constants.cs
@@ -3,7 +3,7 @@
     public static class Constants
     {
         #region Urls
-        public const string NodeJSWindowsBinaryUrl = "http://nodejs.org/dist/v0.12.0/node.exe";
+        public const string NodeJSWindowsBinaryUrl = "http://nodejs.org/dist/v0.12.9/node.exe";
         public const string NPMWindowsZipUrl = "http://nodejs.org/dist/npm/npm-1.4.6.zip";
         #endregion Urls
     }
