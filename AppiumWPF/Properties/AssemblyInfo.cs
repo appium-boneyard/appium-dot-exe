c4b0f28aa2c0827fa0f31ed82ce68bf812c4f3db
astro03 <locopelon03@hotmail.com>
2015-12-08 14:28:39 -0800

    update version number for release

--- AppiumWPF/Properties/AssemblyInfo.cs
+++ AppiumWPF/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.4.13.1")]
-[assembly: AssemblyFileVersion("1.4.13.1")]
+[assembly: AssemblyVersion("1.4.16.1")]
+[assembly: AssemblyFileVersion("1.4.16.1")]
